501a10a2a039da1f839c9f6e5f6d818b3fc52510
krasimirr <krusev0601@gmail.com>
2016-01-27 23:57:47 +0000

    Chapter 4 - 2142896R

--- Chapter 4 - 2142896R/tango_with_django_project/urls.py
+++ Chapter 4 - 2142896R/tango_with_django_project/urls.py
@@ -10,10 +10,3 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
 )
-
-if settings.DEBUG:
-    urlpatterns += patterns(
-        'django.views.static',
-        (r'^media/(?P<path>.*)',
-        'serve',
-        {'document_root': settings.MEDIA_ROOT}), )
