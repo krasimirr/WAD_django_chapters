e26d2b3e43f5e154b93fd974e66295b212665c85
krasimirr <krusev0601@gmail.com>
2016-03-29 05:24:32 +0100

    Chapter 15 - 2142896R

--- Chapter 15 - 2142896R/rango/views.py
+++ Chapter 15 - 2142896R/rango/views.py
@@ -5,6 +5,7 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
+from rango.bing_search import run_query
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -142,3 +143,15 @@ def restricted(request):
     return render(request, 'rango/restricted.html', {})
 
 
+def search(request):
+
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list})
